d81e9627adb8e50cfa67cd0f3474e93f99a52cf5
Alexander Tkachuk <tkachuka2020@gmail.com>
2021-10-02 17:27:32 +0300

    Task was completed

---
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import styles from './FeedbackOptions.module.css'
+
+function FeedbackOptions({ options, onLeaveFeedback }) {
+    return (
+        <ul className={styles.list}>{
+            options.map((option, index) => {
+                return (
+                    <li className={styles.item} key={index}>
+                        <button
+                            className={styles.button}
+                            type="button"
+                            onClick={onLeaveFeedback}
+                        >
+                            {option}
+                        </button>
+                    </li>
+                );
+            })
+        }</ul>
+    );
+}
+
+FeedbackOptions.propTypes = {
+    options: PropTypes.arrayOf(PropTypes.string),
+    onLeaveFeedback: PropTypes.func
+}
+
+export default FeedbackOptions;
